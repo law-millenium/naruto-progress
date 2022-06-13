d772481fe97a5bb27aebfc9c7b98dc1ea70903f1
LHEB <bastien.lahaye@cnieg.fr>
2022-06-13 16:13:49 +0200

    Multiples shinobis added (Itachi, Sakura, Shisui & Tenten). Colors modified for all shinobis. Positions fixed.

--- src/main/java/com/lawmillenium/intellij/plugins/narutoprogress/model/Shinobi.java
+++ src/main/java/com/lawmillenium/intellij/plugins/narutoprogress/model/Shinobi.java
@@ -9,13 +9,17 @@ import com.intellij.openapi.util.text.StringUtil;
 
 public enum Shinobi {
     NARUTO("naruto", -16, 0, 32, ShinobiGroup.KONOHA), //
-    NARUTO_KYUBI("narutoKyubi", -16, -4, 32, ShinobiGroup.KONOHA), //
+    NARUTO_KYUBI("narutoKyubi", -16, -2, 32, ShinobiGroup.KONOHA), //
+    SAKURA("sakura", -16, 0, 32, ShinobiGroup.KONOHA), //
     DRUNK_ROCK_LEE("drunkRockLee", -16, -2, 32, ShinobiGroup.KONOHA), //
+    TENTEN("tenten", -16, -2, 32, ShinobiGroup.KONOHA), //
+    SHISUI("shisui", -16, 0, 32, ShinobiGroup.KONOHA), //
     KURENAI("kurenai", -16, -3, 32, ShinobiGroup.KONOHA), //
     TOBIRAMA("tobirama", -16, 0, 32, ShinobiGroup.KONOHA), //
     MADARA("madara", -16, 0, 32, ShinobiGroup.KONOHA), //
     YUGITO("yugito", -16, 0, 32, ShinobiGroup.KUMO), //
     TOBI("tobi", -16, 0, 32, ShinobiGroup.AKATSUKI), //
+    ITACHI("itachi", -16, -1, 32, ShinobiGroup.AKATSUKI), //
     MISSINGNO("missingNo.", -20, 0, 35, true, null);
 
     public static final Map<String, Shinobi> DEFAULT_SHINOBIS = Arrays.stream(values()).filter(shinobis -> !shinobis.secret).collect(
